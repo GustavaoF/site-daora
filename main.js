1db12fc700267a4210fcc4a713f7285962277012
GustavaoF <gustavo.chaves.almeida@escola.pr.gov.br>
2024-09-25 08:07:54 -0300

    .

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ function soma (num1,num2){
 
 console.log(soma(70,7));
 
-function quadradoarea(num1){
+function areaquadrado(num1){
     return num1*num1;
 }
 
-console.log("Area=" + quadradoarea(7));
+console.log("Area=" + areaquadrado(7));
