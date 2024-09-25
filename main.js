083762099c6674c5422e9a9c09e006f3d9596446
GustavaoF <gustavo.chaves.almeida@escola.pr.gov.br>
2024-09-25 08:09:59 -0300

    .

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ function soma (num1,num2){
 
 console.log(soma(70,7));
 
-function areaQuadrado(num1){
-    return num1*num1;
+function areaQuadrado(lado){
+    return lado**2;
 }
 
 console.log("Area=" + areaQuadrado(7));
