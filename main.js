f6462b386d86e29c507836298e6e3f3888adf91c
GustavaoF <gustavo.chaves.almeida@escola.pr.gov.br>
2024-10-02 07:58:18 -0300

    .

--- main.js
+++ main.js
@@ -23,7 +23,7 @@
 
 let i = 0;
 let estados = ['RS', 'SC', 'PR', 'SP', 'MG', 'RJ', 'ES', 'MT', 'MS', 'GO', 'DF', 'RO', 'AC', 'AM', 'RR', 'PA', 'AP', 'TO', 'MA', 'PI', 'CE', 'RN', 'PE', 'PB', 'AL', 'SE', 'BA']
-while (i<5){
-    console.log(i);
+while (i<27){
+    console.log(estados[i]);
     i=i+1;
 }
